2e3ee36ad6645c1134f3e274c18972c1273d0967
bwwtq75ym4-source <bwwtq75ym4@privaterelay.appleid.com>
2026-03-19 16:14:29 +0100

    Description courte

--- sources/model/game.h
+++ sources/model/game.h
@@ -13,10 +13,18 @@ class Game
 {
 public:
     Game();
+    ~Game();
 
     bool load(std::string const& filename);
 
 private:
+    // vérification des collisions initiales entre les entités
+    bool check_bricks_collision();
+    bool check_paddle_brick();
+    bool check_balls_collision();
+    bool check_ball_brick();
+    bool check_paddle_ball();
+    
     int score;
     int lives;
 
