09bee0595fd6e9e3ab40d20f76b5e34a180e302e
Liam Van Camp <liamvancamp@MacBook-Pro-126.local>
2026-03-28 16:03:33 +0100

    Description courte

--- rendu_projet1/sources/tools/tools.h
+++ rendu_projet1/sources/tools/tools.h
@@ -9,7 +9,7 @@
 
 constexpr double epsil_zero = 0.125;
 
-// définition d'un point (coordonées x et y) , 
+// définition d'un point (coordonées x et y), 
 // mais aussi d'un vecteur (x = déplacement horizontal, y = déplacement vertical)
 
 struct Point
@@ -18,7 +18,7 @@ struct Point
     double y;
 };
 
-// définition d'un cercle par son centre et son rayon 
+// définition d'un cercle par son centre et son rayon
 
 struct Circle
 {
@@ -26,7 +26,7 @@ struct Circle
     double radius;
 };
 
-// définition d'un carré par son centre et la moitié de sa taille 
+// définition d'un carré par son centre et la moitié de sa taille
 // coins :
 // coin_haut_gauche  = (center.x - half_size , center.y + half_size)
 // coin_haut_droit   = (center.x + half_size , center.y + half_size)
@@ -38,9 +38,6 @@ struct Square
     double half_size;
 };
 
-
-
-
 Point operator+(Point a, Point b);
 Point operator-(Point a, Point b);
 Point operator*(Point p, double k);
@@ -50,11 +47,11 @@ Point operator/(Point p, double k);
 // produit scalaire entre deux vecteurs
 double dot(Point a, Point b);
 
-//norme de vecteurs
+// norme de vecteurs
 double norm_carre(Point p);
 double norm(Point p);
 
-//distance entre deux points
+// distance entre deux points
 double distance_carre(Point a, Point b);
 double distance(Point a, Point b);
 
